c0ab2bbbd1c80c30ca1cfd15e8d197f7bf0bdb7a
Sarah Cochrane <sarah.kristine.cochrane@gmail.com>
2023-10-17 21:42:02 -0700

    Add 'automated' gridSquare size

--- javascript.js
+++ javascript.js
@@ -1,15 +1,24 @@
 const gridContainer = document.querySelector(".grid-container");
 
-let squareTotal = 16;
+let squaresPerSide = 16;
 
 function createGridSquares() {
   let gridSquare = document.createElement("div");
   gridSquare.classList.add("grid-square");
+  let squareSize = 500 / squaresPerSide - 1;
+  gridSquare.style.height = squareSize + "px";
+  gridSquare.style.width = squareSize + "px";
   gridContainer.appendChild(gridSquare);
 }
 
-for (let i = 0; i < squareTotal * squareTotal; i++) {
+for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
   createGridSquares();
 }
 
-// CREATE FUNCTION TO DETERMINE SIZE OF GRID SQUARES. USE TO .style.height the GRID SQUARES!!
+// CREATE BUTTON TO PROMPT USER TO PICK SIZE OF GRID -- LOOK AT INSTRUCTIONS FOR PARAMETERS -- OR INPUT BOX DIRECTLY ON PAGE
+
+// CHANGE BACKGROUND COLOR OF GRID SQUARE UPON HOVER
+
+// RESET BUTTON
+
+//BONUS == DARKEN BY 10% EVER HOVER
