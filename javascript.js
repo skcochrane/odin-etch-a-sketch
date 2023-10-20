9948c8a3aa1806e463cfeee2b35425e02b45e3bc
Sarah Cochrane <sarah.kristine.cochrane@gmail.com>
2023-10-19 22:04:47 -0700

    Add button to change grid size

--- javascript.js
+++ javascript.js
@@ -1,21 +1,32 @@
 const gridContainer = document.querySelector(".grid-container");
 
-let squaresPerSide = 16;
-
 function createGridSquares() {
   let gridSquare = document.createElement("div");
   gridSquare.classList.add("grid-square");
-  let squareSize = 500 / squaresPerSide - 1;
+  let squareSize = 450 / squaresPerSide - 1;
   gridSquare.style.height = squareSize + "px";
   gridSquare.style.width = squareSize + "px";
   gridContainer.appendChild(gridSquare);
 }
 
-for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
-  createGridSquares();
-}
+let createGrid = function (squaresPerSide) {
+  for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
+    createGridSquares();
+  }
+};
 
-// CREATE BUTTON TO PROMPT USER TO PICK SIZE OF GRID -- LOOK AT INSTRUCTIONS FOR PARAMETERS -- OR INPUT BOX DIRECTLY ON PAGE
+const changeBtn = document.querySelector(".change-btn");
+changeBtn.addEventListener("click", function () {
+  promptResponse = prompt(
+    "Enter a whole number between 1 and 100 to change the number of squares per side of the grid: "
+  );
+  squaresPerSide = Number(promptResponse);
+  gridContainer.innerHTML = "";
+  gridContainer = createGrid(squaresPerSide);
+});
+
+let squaresPerSide = 16;
+gridContainer = createGrid(squaresPerSide);
 
 // CHANGE BACKGROUND COLOR OF GRID SQUARE UPON HOVER
 
